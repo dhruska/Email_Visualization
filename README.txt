0c02f23f151b2755c6bcf67a0ec184f3d41d6242
Donald Hruska <donald.hruska@icloud.com>
2013-02-04 23:52:14 -0600

    Initial commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+The goal of this project is to visually represent a user's email. In lieu of using my personal email account, I used the Enron corpus located at http://arg.vsb.cz/arg/Enron_Corpus/.
+
+The visualization is made with D3.js (http://d3js.org), and is extended from the Force-Directed Graph implementation (http://bl.ocks.org/4062045).
+
+The Python script jsonbuilder.py, when run, will create the emaildata.json file to be read by the Javascript in index.html.
---
+++ jsonbuilder.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python -tt
+
+import sys
+
+def main():
+    # Check that no parameters were included
+    if len(sys.argv) != 1:
+        print 'usage: ./jsonbuilder.py'
+        sys.exit(1)
+
+    # The employee ID that this program will consider to be the user
+    employeeID = '43906'
+
+    # Open "AllCommunicationList.xml", a list of email interactions of Enron employees
+    f = open('AllCommunicationList.xml', 'rU')
+
+    # Dicts to store the number of emails sent and received to/from each user
+    sent = {}
+    received = {}
+    # Dict for total emails sent/received to each user
+    totalemails = {}
+
+    for line in f:
+        if line[0] == ' ':
+            numbers = line.split('"')
+
+            if numbers[1] == employeeID:
+                sent[numbers[3]] = numbers[5]
+                if numbers[3] in totalemails:
+                    totalemails[numbers[3]] = int(totalemails[numbers[3]]) + int(numbers[5])
+                else:
+                    totalemails[numbers[3]] = numbers[5]
+
+            elif numbers[3] == employeeID:
+                received[numbers[1]] = numbers[5]
+                if numbers[1] in totalemails:
+                    totalemails[numbers[1]] = int(totalemails[numbers[1]]) + int(numbers[5])
+                else:
+                    totalemails[numbers[1]] = numbers[5]
+    f.close()
+
+   
+    # Open json file to write to
+    fw = open('emaildata.json', 'w')
+
+    fw.write('{\n  "nodes":[\n    {"name":"Me","group":1},\n')
+
+    num = 1
+
+    # Write each node
+    for item in totalemails:
+        if num == len(totalemails):
+            fw.write('    {"name":"' + item + '","group":2}\n')
+        else:
+            fw.write('    {"name":"' + item + '","group":2},\n')
+        num = num + 1
+
+    fw.write('  ],\n  "links":[\n')
+
+    num = 1
+
+    # Write each link
+    for item in totalemails:
+        if num == len(totalemails):
+            fw.write('    {"source":0,"target":' + str(num) + ',"value":2}\n')
+        else:
+            fw.write('    {"source":0,"target":' + str(num) + ',"value":2},\n')
+
+
+        num = num + 1
+
+    fw.write('  ]\n}')
+
+    fw.close()
+
+if __name__ == '__main__':
+    main()
